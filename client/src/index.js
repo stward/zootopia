48e6a9c2d2b8743e328230f19bdd0581216ef74c
fresh5447 <doug@kosmojo.com>
2017-02-11 16:28:24 -0700

    basic react router implemention

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+), document.getElementById('root'))
