c4b3faa1f8cac3dc6ac4e11718b1fb262481271a
fresh5447 <doug@kosmojo.com>
2017-02-12 14:17:37 -0700

    add GET signup route that was accidently deleted in previous step

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -30,6 +30,11 @@ module.exports = function(app, passport) {
     // SIGNUP ==============================
     // =====================================
     // show the signup form
+    app.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
