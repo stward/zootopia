06b0a48e0c2e6be7bd6577d9abaa05f048fbd4c5
fresh5447 <doug@kosmojo.com>
2017-02-12 14:12:02 -0700

    update the POST route in userAuth to apply the passport local auth middleware

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -25,11 +25,11 @@ module.exports = function(app, passport) {
     // SIGNUP ==============================
     // =====================================
     // show the signup form
-    app.get('/signup', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // process the signup form
     // app.post('/signup', do all our passport stuff here);
